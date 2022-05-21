b465d8a0944eeaf95917c64fc3502feb137c2ebd
mtthdz <matt@mtthdz.com>
2022-05-21 10:50:29 -0400

    moved redux store to index from app

--- src/App.js
+++ src/App.js
@@ -1,17 +1,11 @@
 import React from "react";
-import { Provider } from 'react-redux';
 import ReduxPage from "./pages/ReduxPage";
-import storeConfig from "./store/storeConfig";
-
-const store = storeConfig();
 
 const App = () => {
   return (
-    <Provider store={ store }>
       <div className="App">
         <ReduxPage />
       </div>
-    </Provider>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App'
+import { Provider } from 'react-redux';
+import storeConfig from "./store/storeConfig";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+const store = storeConfig();
+
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={ store }>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
